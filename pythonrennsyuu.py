f3ee4720113a3e472aad2d63817ce73a358f5d7f
human <human@CAD07.com>
2023-08-29 13:44:08 +0900

    python最初のデータ
    最初のデータ

---
+++ pythonrennsyuu.py
@@ -0,0 +1,16 @@
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(1+1)
+2
+>>> print(100-1)
+99
+
+>>> print("答えは",10+20)
+答えは 30
+>>> print("答えは',10+20)
+      
+SyntaxError: EOL while scanning string literal
+>>> print("答えは"10+20）
+      
+SyntaxError: invalid syntax
+>>> 
